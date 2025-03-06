309f43091dbc85ad729bf1428544bb8b637ad528
Mariann Adelbert <mariannadelbert@gmail.com>
2025-03-06 13:08:20 +0200

    common js vs test model

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import test from './test.js';
+//import test from './test.js';
+const test = require('./test.js').default;
 test.hello();
+
+//import * as common from './common.js';
+console.log(common);
+
+const common = require('./common.js');
+common.hello();
